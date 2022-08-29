385f820d09d3315764badfda3891da53576761b4
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-29 20:37:30 +0300

    JS logic

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function initialize() {
+    fetch("https://random-word-api.herokuapp.com/word?number=1")
+    .then(res => res.json())
+    .then(data => displayWord(data)) 
+}
+
+
+function displayWord(word) {
+console.log(word)
+blanks = document.getElementById('blanks');
+let splitWords =word.map((e) => { return e.split('')})
+
+console.log(splitWords)
+let h = splitWords.map((a) => {
+
+span = document.createElement('span');
+console.log(a)
+span.innerHTML = a;
+blanks.append(span)
+})
+
+} 
+
+initialize()
