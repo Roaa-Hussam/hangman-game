b7b5891d81d88a80d0cb569a3070dc1bfab3b7de
Nijyar Ghazi <nijyar360@gmail.com>
2022-09-02 14:11:25 +0300

    added more Js functionalities Added more Styling

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,10 @@ function initialize() {
     
     fetch("https://random-word-api.herokuapp.com/word?number=1")
         .then(res => res.json())
-        .then(data => {displayWord(data)})
+        .then(data => {
+            displayWord(data)
+            buttonSelector(data)
+        })
 
     }
 
@@ -18,31 +21,62 @@ function displayWord(gotWord) {
     document.querySelector('#blanks').append(blankWords)
     })
 })
-    buttonSelector(gotWord)
  
 }
 
+
+
 function buttonSelector(gotWord) {
+    
     document.querySelectorAll("#letter > button").forEach((indButton)=>{
         indButton.addEventListener('click', ()=>{
-            let selectedLetter = indButton.innerHTML.toLowerCase();
             
-            gotWord.forEach((complWord) => {complWord.split('').forEach((currentLetter,letIndx) => {
-            if (selectedLetter === currentLetter)
-            {
-                console.log(currentLetter)
-                console.log(document.querySelectorAll("#blanks > span")[letIndx])
-                document.querySelectorAll("#blanks > span")[letIndx].innerHTML = selectedLetter;
+            let selectedLetter = indButton.innerHTML.toLowerCase();
+            let found = false
+            if (lives>0){
+            if (indButton.className !== "clicked")
+                {
+                    gotWord.forEach((complWord) => {complWord.split('').forEach((currentLetter,letIndx) => {
+                        if (selectedLetter === currentLetter)
+                        {
+                            console.log(currentLetter)
+                            console.log(document.querySelectorAll("#blanks > span")[letIndx])
+                            document.querySelectorAll("#blanks > span")[letIndx].innerHTML = selectedLetter;
+                            found = true;
+                        }
+                    })})
+
+
+                    if (found==false)
+                    {
+                        lives = lives-1;
+                        screenLives.innerHTML = lives            
+                    }
+                    
+                    indButton.className = "clicked"
+                }
             }
-                })
-            })
-
-        
-
-
+            else{
+                console.log('Game over') 
+            }
         })
     })
+
 }
+
+document.querySelector("#playAgain").addEventListener('click', ()=>{
+    location.reload()
+})
+
+let lives = 10;
+let screenLives = document.createElement('span')
+let yourlives = document.createElement('span')
+
+yourlives.innerHTML = "Lives : "
+screenLives.innerHTML = lives
+
+document.querySelector("#lives").append(yourlives)
+document.querySelector("#lives").append(screenLives)
 
 
 initialize()
